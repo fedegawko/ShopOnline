4ce16a5e79f84b13444f255b3977ebb2ef8ce7a1
fedegawko <96592545+fedegawko@users.noreply.github.com>
2024-02-14 10:54:41 +0100

    Add files via upload

---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await this.shopOnlineDbContext.ProductCategories.ToListAsync();
+            
+            return categories;
+        }
+
+        public async Task<ProductCategory> GetCategory(int Id)
+        {
+            var category = await shopOnlineDbContext.ProductCategories.SingleOrDefaultAsync(c => c.Id == Id);
+            return category;
+        
+        }
+
+        public async Task<Product> Getitem(int id)
+        {
+            var product = await shopOnlineDbContext.Products.FindAsync(id);
+            return product;
+        }
+
+        public Task<Product> GetItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await this.shopOnlineDbContext.Products.ToListAsync();
+
+            return products;
+        }
+    }
+}
